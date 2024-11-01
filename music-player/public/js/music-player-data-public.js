4e14ff564a3c05dfcf1322dccf68a6510b294262
Srdjan <stojanovicsrdjan27@gmail.com>
2024-11-01 10:34:08 +0100

    Create music-player-data-public.js

---
+++ music-player/public/js/music-player-data-public.js
@@ -0,0 +1,61 @@
+
+var assetsPath = musicData.pluginsUrl + '/music-player/public/assets';
+
+
+let songs = [
+    {
+        name: 'song 1',
+        path: ''+assetsPath+'/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: ''+assetsPath+'/images/cover1.png'
+    },
+    {
+        name: 'song 2',
+        path: ''+assetsPath+'/musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: ''+assetsPath+'/images/cover2.png'
+    },
+    {
+        name: 'song 3',
+        path: ''+assetsPath+'/musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: ''+assetsPath+'/images/cover3.png'
+    },
+    {
+        name: 'song 4',
+        path: ''+assetsPath+'/musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: ''+assetsPath+'/images/cover4.png'
+    },
+    {
+        name: 'song 5',
+        path: ''+assetsPath+'/musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: ''+assetsPath+'/images/cover5.png'
+    },
+    {
+        name: 'song 6',
+        path: ''+assetsPath+'/musics/Song 6.mp3',
+        artist: 'artist 6',
+        cover: ''+assetsPath+'/images/cover6.png'
+    },
+    {
+        name: 'song 7',
+        path: ''+assetsPath+'/musics/Song 7.mp3',
+        artist: 'artist 7',
+        cover: ''+assetsPath+'/images/cover7.png'
+    },
+    {
+        name: 'song 8',
+        path: ''+assetsPath+'/musics/Song 8.mp3',
+        artist: 'artist 8',
+        cover: ''+assetsPath+'/images/cover8.png'
+    },
+]
+
+
+
+
+
+
+console.log(songs);
